39aebc9623f3ea421b5facb069d5a7b025301e4e
Bharathy A <bharathypaaruz@gmail.com>
2020-07-15 13:02:27 +0530

    2 msgs

---
+++ fna.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sum();
+void main(){
+int k;
+k=sum();
+printf("res is %d",k);
+}
+int sum(){
+int a,b,c;
+printf("enter 2 numz");
+scanf("%d%d",&a,&b);
+c=a+b;
+return c;
+}
+
